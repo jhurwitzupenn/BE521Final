c30b2c080066bb725690c269f856bd1b40f95b40
Mernies <jamesjwang@gmail.com>
2016-04-22 18:29:06 -0400

    Removed anomalous channels

--- loadTestingData.m
+++ loadTestingData.m
@@ -3,9 +3,16 @@ function [ x ] = loadTestingData( subjectID )
     if subjectID == 1
         load('subject_data.mat','subject1testingData');
         x = subject1testingData;
+        
+        % Remove channel 55
+        x(:,55) = [];
     elseif subjectID == 2
         load('subject_data.mat','subject2testingData');
         x = subject2testingData;
+        
+        % Remove channel 21 and 38
+        x(:,21) = [];
+        x(:,38) = [];
     else
         load('subject_data.mat','subject3testingData');
         x = subject3testingData;
--- loadTrainingData.m
+++ loadTrainingData.m
@@ -3,9 +3,16 @@ function [ x ] = loadTrainingData( subjectID )
     if subjectID == 1
         load('subject_data.mat','subject1trainingData');
         x = subject1trainingData;
+        
+        % Remove channel 55
+        x(:,55) = [];
     elseif subjectID == 2
         load('subject_data.mat','subject2trainingData');
         x = subject2trainingData;
+        
+        % Remove channel 21 and 38
+        x(:,21) = [];
+        x(:,38) = [];
     else
         load('subject_data.mat','subject3trainingData');
         x = subject3trainingData;
