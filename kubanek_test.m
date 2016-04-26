2d874ee465c671f37dede9ac5dce9eeba6d17adc
Mernies <jamesjwang@gmail.com>
2016-04-26 14:23:03 -0400

    Added files via upload

--- kubanek_test.m
+++ kubanek_test.m
@@ -1,35 +1,55 @@
-function kubanek_test(subjectID,CARflag)
+function kubanek_test(subjectID,CARflag, changeFeatures)
 
-%% Load data
-testingData = loadTestingData(subjectID);
-Beta_matrix = loadBetaMatrix(subjectID);
-numChannels = length(testingData(1,:));
-
-%% Pre-process data
-
-if CARflag == 1
-    preProcessedData = meanCAR(testingData, numChannels);
-else
-    preProcessedData = medCAR(testingData, numChannels);
-end
-% preProcessedData = preProcess(testingData, numChannels);
-
-%% Feature Extraction
 % Define window length/displacement
 fs = 1000;
 winLen = 100e-3;    % 100 ms
 winDisp = 50e-3;    % 50 ms
+if changeFeatures == 1
+    fprintf('REQUESTED REBUILD OF TESTING FEATURES\n');
+    %% Load data
+    fprintf('Loading testing data\n');
+    testingData = loadTestingData(subjectID);
+    numChannels = length(testingData(1,:));
 
-% X = BuildFeatures(testingData, fs, winLen, winDisp, numChannels);
-X = BuildFeatures(preProcessedData, fs, winLen, winDisp, numChannels);
+    %% Pre-process data
+
+    if CARflag == 1
+        fprintf('Used mean CAR filter\n');
+        preProcessedData = meanCAR(testingData, numChannels);
+    else
+        fprintf('Used median CAR filter\n');
+        preProcessedData = medCAR(testingData, numChannels);
+    end
+
+    %% Feature Extraction
+    fprintf('Extracting Features\n');
+    X = BuildFeatures(preProcessedData, fs, winLen, winDisp, numChannels);
+    
+    % Cache the features
+    fprintf('Saving Features\n');
+    saveTestFeatures(X, subjectID);
+end
 
 %% Predict and spline interpolation
+
+try
+    X = loadTestFeatures(subjectID);
+catch
+    error('Test features file not found');
+end
+
+try
+    fprintf('Loading Beta matrix\n');
+    Beta_matrix = loadBetaMatrix(subjectID);
+catch
+    error('Beta matrix file not found');
+end
 
 yHat = X*Beta_matrix;
 yPredict = splineInterpolation(yHat, 147500, winDisp);
 
 % Smooth out low amplitude oscillations
-threshold = 0.6;
+threshold = 1.0;
 yPredict2 = yPredict;
 for i = 1:5
     curr_finger = yPredict(:,i);
@@ -38,6 +58,5 @@ for i = 1:5
     yPredict2(idx,i) = smoothed_y(idx);
 end
 
-% savePredictions(yPredict, subjectID);
 savePredictions(yPredict2, subjectID);
 end
